d84609599099bb087a083bd03f2fa9d0596c3e23
Aakamsh PM <aakamshpm@gmail.com>
2023-02-13 17:15:04 +0530

    website

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => (
     p={2} 
     sx={{position: 'sticky', background: '#000', top: 0, justifyContent: 'space-between'}}
     >  
-      <Link to='/' style={{ display: 'flex', 
+      <Link to='/youtube-clone' style={{ display: 'flex', 
       alignItems: 'center'}}>
         <img src={logo} alt="logo" height={45} />
       </Link>
